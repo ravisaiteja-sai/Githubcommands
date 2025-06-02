5a8536d26b332372925b6ddbb74fdc780499db1b
ravisaiteja-sai <ravisaiteja15@gmail.com>
2025-06-02 14:32:09 +0530

    Update Democlass.java

--- src/test/java/gitbashcommands/Democlass.java
+++ src/test/java/gitbashcommands/Democlass.java
@@ -15,5 +15,13 @@ public class Democlass {
 	public void m1() {
 		System.out.println("m1 run is sucessfully");
 	}
-
+	
+public void m2() {
+		System.out.println("m2 run is sucessfully");
+	}
+	public void m3() {
+		System.out.println("m3 run is sucessfully");
+	}
+	
+	
 }
